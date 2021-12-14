432f166972d982dd9f2f640a717e983a6ee7b6fc
Rebecca Solcia <74981737+RebSolcia@users.noreply.github.com>
2021-12-14 15:32:02 +0100

    Updated Ticket Contract to call fake functions

--- test_env/build/contracts/TicketNew.sol
+++ test_env/build/contracts/TicketNew.sol
@@ -44,7 +44,6 @@ contract ChainlinkExample is ChainlinkClient {
         address payable owner;
         uint train_number;
         uint price;
-        uint datetime_departure;
         uint datetime_arrival_predicted;
         string station_departure;
         string station_arrival;
@@ -59,17 +58,21 @@ contract ChainlinkExample is ChainlinkClient {
     
     // The function below is meant to request and store the information from Italo's website
     function requestInfo(string memory _stationDeparture, string memory _stationArrival,
-                         string memory _datetimeDeparture) public returns (bytes32 requestId) {
+                         string memory _trainNumber, string memory _datetimeDeparture,
+                         string memory _datetimeArrivalPredicted, string memory _price) public returns (bytes32 requestId) {
 
         //create a variable and store it temporarily in memory
         Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
 
         //set the url to perform the GET request so that the request is built adaptively
         string memory query = string(abi.encodePacked("https://fd5e-2001-b07-a3c-400a-b561-bc22-61b9-a720.ngrok.io", 
-                                                      "/request_info_mock/?", 
+                                                      "/fake_ticket_search/?", 
                                                       "departure_station=", _stationDeparture, 
                                                       "&arrival_station=", _stationArrival, 
-                                                      "&departure_hour=", _datetimeDeparture));
+                                                      "&train_number=", _trainNumber,
+                                                      "&time_of_departure=", _datetimeDeparture, 
+                                                      "&time_of_arrival=", _datetimeArrivalPredicted, 
+                                                      "&price=", _price));
         request.add("get", query);
 
         //set the path to find the requred data in the api response
@@ -82,11 +85,6 @@ contract ChainlinkExample is ChainlinkClient {
 
         //send the request
         return sendChainlinkRequestTo(Oracle, request, fee);
-    }
-
-    function TurnCounterIntoKeccak(string memory _counter) public pure returns (bytes32) {
-        bytes32 kecca_hashed_counter = keccak256(abi.encode(_counter));
-        return (kecca_hashed_counter);
     }
     
     function fulfill(bytes32 _requestId, bytes32 _response) public recordChainlinkFulfillment(_requestId) {
@@ -105,73 +103,10 @@ contract ChainlinkExample is ChainlinkClient {
         RequestToPrice[counter_key] = response_list_info;
     }
 
-    // The function below is meant to request and store the information from Italo's website
-    function checkDelay(string memory _trainNumber) public returns (bytes32 requestId) {
-
-        //create a variable and store it temporarily in memory
-        Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill_delay.selector);
-
-        //set the url to perform the GET request so that the request is built adaptively
-        string memory query = string(abi.encodePacked("https://fd5e-2001-b07-a3c-400a-b561-bc22-61b9-a720.ngrok.io", 
-                                                      "/check_delay/?", 
-                                                      "train_number=", _trainNumber));
-        request.add("get", query);
-
-        //set the path to find the requred data in the api response
-        request.add("path", "response");
-
-        //send the request
-        return sendChainlinkRequestTo(Oracle, request, fee);
-    }
-    
-    function fulfill_delay(bytes32 _requestId, bytes32 _response) public recordChainlinkFulfillment(_requestId) {
-        // Parse the response as "trainnumber_datetimearrivalpredicted_minutesofdelay"
-        bytesdirisposta = _response;
-
-        // Store the response_string inside a variable, after having transformed the bytes32 response into a string
-        string memory response_string_delay = Converter.bytes32ToString(_response);
-
-        // Split the string response_string based on underscores, so that you can index the response inside 
-        // the RequestToPrice mapping
-        string[] memory response_list_delay = Converter.split(response_string_delay, "_");
-
-        uint _trainNumber_Delay = Converter.StringToUint(response_list_delay[0]);
-        uint _datetimeArrivalPredicted_Delay = Converter.StringToUint(response_list_delay[1]);
-        uint _minutesOfDelay = Converter.StringToUint(response_list_delay[2]);
-        uint length_ticketlist = TicketsByTrainNumberByDatetime[_trainNumber_Delay][_datetimeArrivalPredicted_Delay].length;
-
-        // Check by how many minutes the train has delayed
-        if (_minutesOfDelay > 10) {
-            for (uint i=0; i < length_ticketlist; i++){
-                Ticket memory this_ticket = TicketsByTrainNumberByDatetime[_trainNumber_Delay][_datetimeArrivalPredicted_Delay][i];
-                address payable owner_to_be_repaid = this_ticket.owner;
-                uint amount_to_be_repaid = ((this_ticket.price * 10) / 100) - ((this_ticket.price * 10) % 100);
-                owner_to_be_repaid.transfer(amount_to_be_repaid);
-            }
-
-            emit TicketInfo("The tickets ");
-
-        } else if (_minutesOfDelay > 60) {
-            for (uint i=0; i < length_ticketlist; i++){
-                Ticket memory this_ticket = TicketsByTrainNumberByDatetime[_trainNumber_Delay][_datetimeArrivalPredicted_Delay][i];
-                address payable owner_to_be_repaid = this_ticket.owner;
-                uint amount_to_be_repaid = ((this_ticket.price * 60) / 100) - ((this_ticket.price * 60) % 100);
-                owner_to_be_repaid.transfer(amount_to_be_repaid);
-            }
-
-        } else if (_minutesOfDelay > 90) {
-            for (uint i=0; i < length_ticketlist; i++){
-                Ticket memory this_ticket = TicketsByTrainNumberByDatetime[_trainNumber_Delay][_datetimeArrivalPredicted_Delay][i];
-                address payable owner_to_be_repaid = this_ticket.owner;
-                uint amount_to_be_repaid = ((this_ticket.price * 90) / 100) - ((this_ticket.price * 90) % 100);
-                owner_to_be_repaid.transfer(amount_to_be_repaid);
-            }
-        }
-
-        delete TicketsByTrainNumberByDatetime[_trainNumber_Delay][_datetimeArrivalPredicted_Delay];
-
-        // Store the response_string inside a variable, after having transformed the bytes32 response into a string
-        response_string = Converter.bytes32ToString(_response);
+    // Function used to turn strings into keccak bytes32
+    function TurnCounterIntoKeccak(string memory _counter) public pure returns (bytes32) {
+        bytes32 kecca_hashed_counter = keccak256(abi.encode(_counter));
+        return (kecca_hashed_counter);
     }
 
     // Function buyTicket enters into action when a call has been made to the webscraper. 
@@ -195,17 +130,88 @@ contract ChainlinkExample is ChainlinkClient {
         string memory _stationArrival = RequestToPrice[_requestId][2];
         uint _trainNumber = Converter.StringToUint(RequestToPrice[_requestId][3]);
         uint _price = Converter.StringToUint(RequestToPrice[_requestId][4]);
-
-        uint _datetimeDeparture = 14;
         
         // Emit an event telling the user in which case he's supposed to ask for a refund 
         emit TicketInfo(string(abi.encodePacked("You have successfully bought your ticket from ", _stationDeparture, " to ", _stationArrival, ". The train ", _trainNumber ," is scheduled to arrive at ", _datetimeArrivalPredicted, ". Make sure to ask for a refund in case of delay! Thanks for choosing our service")));
 
         // Push the Ticket inside the Ticket array, given train number and given the datetime
-        TicketsByTrainNumberByDatetime[_trainNumber][_datetimeArrivalPredicted].push(Ticket(_owner, _trainNumber, _price, _datetimeDeparture, _datetimeArrivalPredicted, _stationDeparture,  _stationArrival));
+        TicketsByTrainNumberByDatetime[_trainNumber][_datetimeArrivalPredicted].push(Ticket(_owner, _trainNumber, _price, _datetimeArrivalPredicted, _stationDeparture,  _stationArrival));
 
         // Remove the RequestId from the RequestToPrice mapping to avoid storing useless stuff
         delete RequestToPrice[_requestId];
+    }
+
+
+    // The function below is meant to request and store the information from Italo's website
+    function checkDelay(string memory _trainNumber, string memory _expectedArr,
+                        string memory _delay, string memory _thr) public returns (bytes32 requestId) {
+
+        //create a variable and store it temporarily in memory
+        Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill_delay.selector);
+
+        //set the url to perform the GET request so that the request is built adaptively
+        string memory query = string(abi.encodePacked("https://fd5e-2001-b07-a3c-400a-b561-bc22-61b9-a720.ngrok.io", 
+                                                      "/fake_delay/?", 
+                                                      "train_number=", _trainNumber,
+                                                      "&expected_arr=", _expectedArr,
+                                                      "&delay=", _delay,
+                                                      "&thr=", _thr));
+        request.add("get", query);
+
+        //set the path to find the requred data in the api response
+        request.add("path", "response");
+
+        //send the request
+        return sendChainlinkRequestTo(Oracle, request, fee);
+    }
+    
+    function fulfill_delay(bytes32 _requestId, bytes32 _response) public recordChainlinkFulfillment(_requestId) {
+        // Parse the response as "trainnumber_datetimearrivalpredicted_minutesofdelay"
+        bytesdirisposta = _response;
+        // Store the response_string inside a variable, after having transformed the bytes32 response into a string
+        response_string = Converter.bytes32ToString(_response);
+
+        // Store the response_string inside a variable, after having transformed the bytes32 response into a string
+        string memory response_string_delay = Converter.bytes32ToString(_response);
+
+        // Split the string response_string based on underscores, so that you can index the response inside 
+        // the RequestToPrice mapping
+        string[] memory response_list_delay = Converter.split(response_string_delay, "_");
+
+        string memory boolean = response_list_delay[0];
+        uint _trainNumber_Delay = Converter.StringToUint(response_list_delay[1]);
+        uint _datetimeArrivalPredicted_Delay = Converter.StringToUint(response_list_delay[2]);
+        uint _minutesOfDelay = Converter.StringToUint(response_list_delay[3]);
+        uint length_ticketlist = TicketsByTrainNumberByDatetime[_trainNumber_Delay][_datetimeArrivalPredicted_Delay].length;
+
+        // Check by how many minutes the train has delayed
+        if (keccak256(abi.encodePacked(boolean))==keccak256(abi.encodePacked("True"))) {
+            if (_minutesOfDelay > 10) {
+                for (uint i=0; i < length_ticketlist; i++){
+                    Ticket memory this_ticket = TicketsByTrainNumberByDatetime[_trainNumber_Delay][_datetimeArrivalPredicted_Delay][i];
+                    address payable owner_to_be_repaid = this_ticket.owner;
+                    uint amount_to_be_repaid = ((this_ticket.price * 10) / 100) - ((this_ticket.price * 10) % 100);
+                    owner_to_be_repaid.transfer(amount_to_be_repaid);
+                }
+            } else if (_minutesOfDelay > 60) {
+                for (uint i=0; i < length_ticketlist; i++){
+                    Ticket memory this_ticket = TicketsByTrainNumberByDatetime[_trainNumber_Delay][_datetimeArrivalPredicted_Delay][i];
+                    address payable owner_to_be_repaid = this_ticket.owner;
+                    uint amount_to_be_repaid = ((this_ticket.price * 60) / 100) - ((this_ticket.price * 60) % 100);
+                    owner_to_be_repaid.transfer(amount_to_be_repaid);
+                }
+
+            } else if (_minutesOfDelay > 90) {
+                for (uint i=0; i < length_ticketlist; i++){
+                    Ticket memory this_ticket = TicketsByTrainNumberByDatetime[_trainNumber_Delay][_datetimeArrivalPredicted_Delay][i];
+                    address payable owner_to_be_repaid = this_ticket.owner;
+                    uint amount_to_be_repaid = ((this_ticket.price * 90) / 100) - ((this_ticket.price * 90) % 100);
+                    owner_to_be_repaid.transfer(amount_to_be_repaid);
+                }
+            }
+        }
+
+        delete TicketsByTrainNumberByDatetime[_trainNumber_Delay][_datetimeArrivalPredicted_Delay];
     }
 
 }
