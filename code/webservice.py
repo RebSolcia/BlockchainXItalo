4187fc9924009d2c3dd05a2842e6bd006a8e191b
steffenbrockmann <contact.brockmann@gmail.com>
2021-12-15 01:11:29 +0100

    commenting code

--- code/webservice.py
+++ code/webservice.py
@@ -19,12 +19,11 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
-# Change the following path
+# IMPORTANT: Change the following path to local chromedriver instance
 exe_path = os.path.join(os.getcwd(), "chromedriver.exe")
 ser = Service(exe_path)
 
 # Instantiate the first variables and exception handlers
-
 VERSION = "0.0.1"
 
 logger = logging.getLogger(__name__)
@@ -32,22 +31,23 @@ logger = logging.getLogger(__name__)
 tags_metadata = [
     {
         "name": "searchTicket",
-        "description": "Endpoints that allows the search of tickets."
+        "description": "Endpoint that allows the search of tickets and retrieval of ticket information."
     },
     {
         "name": "checkDelay",
-        "description": "Endpoints that allows to check if a train had a delay of more than 60 minutes."
+        "description": "Endpoint that allows checking if a train had a delay of more than 60 minutes."
     },
     {
         "name": "fakeTicketSearch",
-        "description": "Endpoints that allows to simulate the response obtained by calling the searchTicket function."
+        "description": "Endpoint that allows to simulate the response obtained by calling the searchTicket function."
     },
     {
         "name": "fakeDelay",
-        "description": "Endpoints that allows to simulate the response obtained by calling the checkDelay function."
+        "description": "Endpoint that allows to simulate the response obtained by calling the checkDelay function."
     }
 ]
 
+# Intitialize FastAPI 
 app = FastAPI(
     title="BlockchainXItalo",
     description="A webservice that interacts with Italo through use of ChainLink and Ethereum blockchain.",
@@ -92,6 +92,8 @@ async def validation_exception_handler(
     summary="Search for Tickets",
     description="Main method to search for tickets on Italo's website."
 )
+
+# Defininig search_ticket function which simulates the purchasing of a ticket through scraping Italo's website 
 async def search_ticket(
         request: Request,
 
@@ -109,28 +111,32 @@ async def search_ticket(
         ),
 ):
     # Scrape Italo's website to get first available train after the specified hour
-    train_URL = 'https://www.italotreno.it/en/destinations-timetable/trains-schedules'
+    train_URL = 'https://www.italotreno.it/en/destinations-timetable/trains-schedules' 
 
-    options = webdriver.ChromeOptions()
+    # Start webdriver on Italo's Website
+    options = webdriver.ChromeOptions() 
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option('useAutomationExtension', False)
 
     driver = webdriver.Chrome(service=ser,
                               options=options)  # or webdriver.Chrome(executable_path=local_path_of_driver, options=options)
     driver.get(train_URL)
-
+    
+    # Input requested departure station and arrival station
     search_box_from = driver.find_element_by_xpath(
         '/html/body/main/section[3]/div[2]/form/div[1]/div/table/tbody/tr[1]/td[1]/fieldset/div/input[1]').send_keys(
         departure_station)
     search_box_to = driver.find_element_by_xpath(
         '/html/body/main/section[3]/div[2]/form/div[1]/div/table/tbody/tr[1]/td[3]/fieldset/div/input[1]').send_keys(
         arrival_station)
-
+    
+    # Search for train connections
     button = driver.find_element_by_xpath(
         '/html/body/main/section[3]/div[2]/form/div[1]/div/table/tbody/tr[1]/td[4]/div/a')
     webdriver.ActionChains(driver).click_and_hold(button).perform()
     driver.execute_script("arguments[0].click();", button)
-
+    
+    # Wait for page to be loaded
     try:
         element_present = EC.presence_of_element_located(
             (By.XPATH, '/html/body/main/section[3]/div[3]/div[2]/table/tbody/tr[1]/td[1]/p[1]'))
@@ -141,7 +147,8 @@ async def search_ticket(
     num_options = len(driver.find_elements_by_xpath("/html/body/main/section[3]/div[3]/div[2]/table/tbody/tr"))
     if num_options == 0:
         return f"Sorry, no routes available from {departure_station} to {arrival_station}"
-
+    
+    # Choose best train connection depending on requested time
     timing = []
     departure_hour = str(departure_hour) + "00"
     for i in range(1, num_options + 1):
@@ -158,7 +165,8 @@ async def search_ticket(
 
     x = 0
     choice = timing[x][0]
-
+    
+    # Converting time of departure and time of arrival of connection to unix format
     time_of_departure, time_of_arrival = timing[x][1:]
     today = datetime.date.today()
     day_dep = today.day + 1
@@ -170,15 +178,19 @@ async def search_ticket(
     datetime_arr = datetime.datetime(today.year, today.month, day_arr, int(time_of_arrival.split(":")[0]),
                                      int(time_of_arrival.split(":")[1]))
     unix_arrival = round(time.mktime(datetime_arr.timetuple()))
-
+    
+    # Retrieve train number
     train_number = driver.find_element_by_xpath(
         f'/html/body/main/section[3]/div[3]/div[2]/table/tbody/tr[{choice}]/td[4]/p[2]').get_attribute("innerText")
-
+    
+    # Shorten departure station and arrival station for output
     departure_station_new = departure_station[:2]
     arrival_station_new = arrival_station[:2]
-
+    
+    # Choose random price between 50 and 100 since retrieving real price would have resulted in chainlink timeout due to slow loading response time of website
     price = np.random.randint(50, 100)
-
+    
+    # fill ticket with information
     ticket = f"{departure_station_new}_{unix_arrival}_{arrival_station_new}_{train_number}_{price}"
 
     return {"response": ticket}
@@ -190,6 +202,8 @@ async def search_ticket(
     summary="Fake Ticket Search",
     description="Main method to simulate response from calling the searchTicket function."
 )
+
+# Simulation of search_ticket where every piece of ticket information can be chosenn by user and is returned in correct format 
 async def fake_ticket_search(
         request: Request,
 
@@ -219,6 +233,8 @@ async def fake_ticket_search(
             description="Price of one ticket (e.g. 59.90)."
         )
 ):
+    
+    # Converting time of departure and time of arrival of connection to unix format
     today = datetime.date.today()
     day_dep = today.day + 1
     if (int(time_of_arrival.split(":")[0]) - int(time_of_departure.split(":")[0])) < 0:
@@ -228,7 +244,7 @@ async def fake_ticket_search(
     datetime_arr = datetime.datetime(today.year, today.month, day_arr, int(time_of_arrival.split(":")[0]),
                                      int(time_of_arrival.split(":")[1]))
     unix_arrival = round(time.mktime(datetime_arr.timetuple()))
-
+    
     price_rounded = int(float(price[:-2]))
 
     departure_station_new = departure_station[:2]
@@ -245,6 +261,8 @@ async def fake_ticket_search(
     summary="Check Delay",
     description="Main method to check if a train had a delay of more than 60 minutes."
 )
+
+# Checking if train is delayed by train number through webscraping
 async def check_delay(
         request: Request,
 
@@ -257,15 +275,17 @@ async def check_delay(
             description="Unix datetime of expected arrival."
         )
 ):
-    # Scrape Italo's website to check if train has a delay
+    # Initialize webdriver on Italo website
     driver = webdriver.Chrome(service=ser)  # or webdriver.Chrome(executable_path=local_path_of_driver)
     base_URL = 'https://italoinviaggio.italotreno.it/en/train'
     driver.get(base_URL + '/' + str(train_number))
 
+    # Check if train is currently running and delayed
+    delay_thresh = 30 # threshhold for which function checks if delayed
     try:
         delay = driver.find_element_by_xpath(
             '/html/body/div[2]/section/div/div/div[1]/div/div/div[3]/span[2]').get_attribute("innerText")
-        if (int(delay.split()[0])) >= 30:
+        if (int(delay.split()[0])) >= delay_thresh:
             return {"response": f"True_{train_number}_{expected_arr}_{delay}"}
         else:
             return {"response": f"False_{train_number}_{expected_arr}_{delay}"}
@@ -279,6 +299,8 @@ async def check_delay(
     summary="Fake Delay",
     description="Main method to simulate response from calling the checkDelay function."
 )
+
+# Simulation of check_delay function where user can input train number and length of delay
 async def fake_delay(
         request: Request,
         train_number: str = Query(
